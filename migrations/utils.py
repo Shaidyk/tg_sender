3ba2e02b30bcb97db19aae53b7f8fa1aae869966
Andrey Shaidyk <andrey.shaidyk@arixess.com>
2024-05-15 10:31:44 +0200

    fix docker

--- migrations/utils.py
+++ migrations/utils.py
@@ -1,26 +1,27 @@
 from asyncpg import connect as async_pg_connect, InvalidCatalogNameError
+from app.config import settings
 
 
 # Database creation.
 async def create_database_if_not_exists(database_name: str):
     try:
         conn = await async_pg_connect(
-            host="db",
-            port=5432,
-            user="admin",
-            password="admin",
-            database="tg_sender_db"
+            host=settings.POSTGRES_DB,
+            port=settings.POSTGRES_PORT,
+            user=settings.POSTGRES_USER,
+            password=settings.POSTGRES_PASSWORD,
+            database=settings.POSTGRES_DB
         )
     except InvalidCatalogNameError:
         sys_conn = await async_pg_connect(
-            host="db",
-            port=5432,
-            user="admin",
-            password="admin",
-            database='postgres'
+            host=settings.POSTGRES_DB,
+            port=settings.POSTGRES_PORT,
+            user=settings.POSTGRES_USER,
+            password=settings.POSTGRES_PASSWORD,
+            database=settings.POSTGRES_DB
         )
         await sys_conn.execute(
-            f'CREATE DATABASE "{database_name}" OWNER "{"admin"}"'
+            f'CREATE DATABASE "{database_name}" OWNER "{settings.POSTGRES_USER}"'
         )
         await sys_conn.close()
     else:
